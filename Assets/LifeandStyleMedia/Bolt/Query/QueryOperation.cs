41ba95988187031b7cfbab68df8e8c1910d2e420
Goy288 <44177200+Goy288@users.noreply.github.com>
2020-08-12 10:36:47 -0700

    Added an extension for Bolt & a TransformScaleFlip macro

---
+++ Assets/LifeandStyleMedia/Bolt/Query/QueryOperation.cs
@@ -0,0 +1,17 @@
+﻿namespace Lasm.BoltExtensions
+{
+    /// <summary>
+    /// An enum of Linq query operations.
+    /// </summary>
+    public enum QueryOperation
+    {
+        Any,
+        AnyWithCondition,
+        First,
+        FirstOrDefault,
+        OrderBy,
+        OrderByDescending,
+        Single,
+        Where
+    }
+}
